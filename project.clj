990fef405a37299ea2006b25c2ed5683ddade347
nvbn <nvbn.rm@gmail.com>
2015-03-18 16:44:40 +0100

    Update project dependencies

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2850"]
-                 [figwheel "0.2.5-SNAPSHOT"]
+                 [org.clojure/clojurescript "0.0-3123"]
+                 [figwheel "0.2.5"]
                  [rm-hull/monet "0.2.1"]
                  [jayq "2.5.4"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
